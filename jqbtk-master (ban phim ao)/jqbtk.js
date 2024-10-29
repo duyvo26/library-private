c24dbf976f303f4db42ca24e21e711ed25e5ff5e
duyvo26 <duyvo1945@gmail.com>
2024-10-29 11:35:47 +0700

    new update ban phim ao

--- jqbtk-master (ban phim ao)/jqbtk.js
+++ jqbtk-master (ban phim ao)/jqbtk.js
@@ -233,13 +233,13 @@
                             btn.attr('data-value', this[0]).attr('data-value-alt', this[1]);
                             switch (this[0]) {
                                 case 'shift':
-                                    btn.addClass('jqbtk-shift').html('<span class="glyphicon glyphicon-arrow-up"></span>');
+                                    btn.addClass('jqbtk-shift').html('<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-shift" viewBox="0 0 16 16"> <path d="M7.27 2.047a1 1 0 0 1 1.46 0l6.345 6.77c.6.638.146 1.683-.73 1.683H11.5v3a1 1 0 0 1-1 1h-5a1 1 0 0 1-1-1v-3H1.654C.78 10.5.326 9.455.924 8.816zM14.346 9.5 8 2.731 1.654 9.5H4.5a1 1 0 0 1 1 1v3h5v-3a1 1 0 0 1 1-1z"/> </svg>');
                                     break;
                                 case 'space':
-                                    btn.addClass('jqbtk-space').html('&nbsp;');
+                                    btn.addClass('jqbtk-space').html('<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-distribute-vertical" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1 1.5a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 0-1h-13a.5.5 0 0 0-.5.5m0 13a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 0-1h-13a.5.5 0 0 0-.5.5"/> <path d="M2 7a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/> </svg>');
                                     break;
                                 case 'del':
-                                    btn.addClass('jqbtk-del').html('<span class="glyphicon glyphicon-arrow-left"></span>');
+                                    btn.addClass('jqbtk-del').html('<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/> <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/> </svg>');
                                     break;
                                 case 'enter':
                                     btn.addClass('jqbtk-enter').html('Enter');
